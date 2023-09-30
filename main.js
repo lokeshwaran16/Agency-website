876181bc1dcaa9163470d16a395c1a0e874466c3
lokeshwaran16 <112813991+lokeshwaran16@users.noreply.github.com>
2023-10-01 00:42:43 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function menuClick() {
+    var menu = document.getElementById('mobileMenu');
+    if (menu.classList.contains('hidden')) {
+      menu.classList.remove('hidden');
+    }
+      else{
+
+        menu.classList.add('hidden');
+   
+    }
+  }
